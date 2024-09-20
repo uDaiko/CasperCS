ca9724552848bcb6991728260faf91f34b385fd8
uDaiko <uDegr@proton.me>
2024-09-20 02:32:28 +0200

    installed and commited needed modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@nuxtjs/supabase', '@nuxtjs/google-fonts', ],
+  googleFonts: {
+    families: {
+      'Inter': [100, 200, 300, 400, 500, 600, 700]
+    },
+  },
 })
