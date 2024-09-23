77a0a4533d7f12fc34672bcf480b989ad42663c7
uDaiko <uDegr@proton.me>
2024-09-23 18:45:30 +0200

    added summary component to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import type { InvestmentCollection } from '~/types';
+
+const summaries: InvestmentCollection[] = [
+    {
+        type: 'Crypto',
+        name: 'Default Crypto Tracking',
+
+    },
+    {
+        type: 'Stocks',
+        name: 'Default Stock Tracking'
+
+    }
+]
+</script>
+
 <template>
-    <div>
-        
-    </div>
+    <main class="container mx-auto px-4 py-8">
+        <section class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+            <div v-for="summary in summaries" :key="summary.name">
+                <InvestmentSummary :type="summary.type" :name="summary.name" />
+            </div>
+        </section>
+    </main>
 </template>
