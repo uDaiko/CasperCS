83c04758852d324522550806b2954b4e7e8b0210
uDaiko <uDegr@proton.me>
2024-09-25 08:57:00 +0200

    reworked to typescript and fixed errors

--- composables/useFetchCoinPrice.ts
+++ composables/useFetchCoinPrice.ts
@@ -1,6 +1,5 @@
-export const useFetchCoinPrice = (coin) => {
+export const useFetchCoinPrice = (coin: string) => {
   const config = useRuntimeConfig();
-  const transactions = ref([]);
   const pending = ref(false);
   const toast = useToast();
   const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd&x_cg_demo_api_key=${config.secretApiKey}`;
@@ -9,12 +8,10 @@ export const useFetchCoinPrice = (coin) => {
     pending.value = true;
 
     try {
-      const { data } = await useAsyncData("coinData", () => {
-        () => $fetch(url);
-      });
+      const { data } = await useAsyncData("coinData", () => $fetch(url));
 
       return data;
-    } catch {
+    } catch (e: any) {
       toast.add({
         title: "coin data could not be fetched",
         description: e.message,
@@ -24,5 +21,10 @@ export const useFetchCoinPrice = (coin) => {
     } finally {
       pending.value = false;
     }
+  };
+
+  return {
+    fetchCoin,
+    pending,
   };
 };
