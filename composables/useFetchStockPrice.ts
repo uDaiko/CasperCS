114b85f91f31cf7c785b8b1cad118777c2f93e7f
uDaiko <uDegr@proton.me>
2024-09-29 00:44:53 +0200

    composable for fetching stock prices

---
+++ composables/useFetchStockPrice.ts
@@ -0,0 +1,19 @@
+export const useFetchStockPrice = (stock: string) => {
+  const url = `https://api.polygon.io/v2/aggs/ticker/${stock}/prev?appiKey=${process.env.STOCKAPI_KEY}`;
+
+  const fetchStockPrice = async () => {
+    const { data, error } = await useAsyncData("stockData", () => $fetch(url));
+
+    if (error.value) {
+      console.log("could not fetch");
+
+      return { error: error.value };
+    }
+    console.log("this is the return in comp");
+
+    return { data: data.value };
+  };
+  return {
+    fetchStockPrice,
+  };
+};
