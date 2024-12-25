934e01df09d7ec2af583176a15953cb32543c05d
uDaiko <uDegr@proton.me>
2024-12-25 22:59:26 +0900

    renamed and reststructured file

---
+++ components/asset-table.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const props = defineProps({
+    assetData:Object
+})
+
+let stockDataRow = []
+
+</script>
+
+<template>
+  <div>{{ assetData }}</div>
+  
+  <UTable> 
+
+    
+  </UTable>
+</template>
