2b7d9e9ad91e656373f83883117bd48e1f55cc8a
uDaiko <uDegr@proton.me>
2024-12-26 07:32:08 +0900

    composable for calculating holding total

--- composables/useFetchStockPrice.ts
+++ composables/useFetchStockPrice.ts
@@ -1,21 +1,36 @@
-export const useFetchStockPrice = (stock: string) => {
-  const url = `https://api.polygon.io/v2/aggs/ticker/${stock}/prev?appiKey=${process.env.STOCKAPI_KEY}`;
-  const supabase = useSupabaseClient()
-  const user = useSupabaseUser()
+type StockApiResponse = {
+  results: Array<{
+    c: number; // c represents the closing price of the previous day
+  }>;
+};
+
+export const useCalculateStockTotal = (asset) => {
+  const url = computed(
+    () =>
+      `https://api.polygon.io/v2/aggs/ticker/${asset.name}/prev?apiKey=${process.env.STOCKAPI_KEY}`
+  );
+  const supabase = useSupabaseClient();
+  const user = useSupabaseUser();
+
+  const lastClosePrice = ref(null);
+
+  const total = computed(() => {
+    if (!lastClosePrice.value) return null;
+    return lastClosePrice.value * asset.amount;
+  });
 
   const fetchStockPrice = async () => {
-    const { data, error } = await useAsyncData("stockData", () => $fetch(url));
+    const { data, error } = await useFetch<StockApiResponse>(url.value);
 
     if (error.value) {
-      console.log("could not fetch");
-
-      return { error: error.value };
+      console.error("Error fetching stock price:", error.value.message);
+      return;
     }
-    console.log("this is the return in comp");
 
-    return { data: data.value };
+    lastClosePrice.value = data.value.results[0].c;
   };
+  fetchStockPrice();
   return {
-    fetchStockPrice,
+    total,
   };
 };
