d7398af9db1e3474066373e6b536bda275448203
uDaiko <uDegr@proton.me>
2024-12-29 03:49:56 +0900

    table styling & sorting

--- components/asset-table.vue
+++ components/asset-table.vue
@@ -5,11 +5,39 @@ const props = defineProps({
   assetData: Object,
 });
 
-let stockDataRow: StockRow[] = [];
+const columns = [
+  {
+    key: "id",
+    label: "ID",
+    sortable: true,
+  },
+  {
+    key: "ticker",
+    label: "Ticker",
+  },
+  {
+    key: "amount",
+    label: "Amount",
+    sortable: true,
+  },
+  {
+    key: "total",
+    label: "Total",
+    sortable: true,
+  },
+];
+
+let stockDataRows: StockRow[] = [];
+stockDataRows = props.assetData.map((item) => ({
+  id: item.id,
+  name: item.ticker,
+  amount: item.amount,
+  total: item.total,
+}));
 </script>
 
 <template>
   <div>{{ assetData }}</div>
 
-  <UTable> </UTable>
+  <UTable :columns="columns" :rows="stockDataRows"> </UTable>
 </template>
