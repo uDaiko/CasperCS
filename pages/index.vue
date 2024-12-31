82eee61890e45f69e71eb507afb176ed66b3a0cb
uDaiko <uDegr@proton.me>
2024-12-31 16:10:54 +0900

    handling of dynamically fetched pf data from db

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import type { InvestmentCollection } from '~/types';
+import type { InvestmentPortfolio } from "~/types";
 
-const summaries: InvestmentCollection[] = [
-    {
-        type: 'Crypto',
-        name: 'Default Crypto Tracking',
+const portfolios = ref<InvestmentPortfolio[]>([]);
+const { fetchStockPortfolios } = useFetchPortfolios();
 
-    },
-    {
-        type: 'Stocks',
-        name: 'Default Stock Tracking'
-
-    }
-]
+onBeforeMount(async () => {
+  const data = await fetchStockPortfolios();
+  if (data) {
+    console.log("this is portfolios.value");
+    console.log(portfolios.value);
+    portfolios.value = data;
+    console.log("this is portfolios.value");
+    console.log(portfolios.value);
+  }
+});
 </script>
 
 <template>
-    <main class="container mx-auto px-4 py-8">
-        <section class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-            <div v-for="summary in summaries" :key="summary.name">
-                <router-link :to="`/portfolio/${summary.name}`">
-                    <InvestmentSummary :type="summary.type" :name="summary.name" />
-                </router-link>
-            </div>
-            
-        </section>
-        <NewPortfolio />
-    </main>
+  <main class="container mx-auto px-4 py-8">
+    <section class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+      <div v-for="portfolio in portfolios" :key="portfolio.id">
+        <router-link :to="`/portfolio/${portfolio.name}`">
+          <InvestmentSummary :type="portfolio.type" :name="portfolio.name" />
+        </router-link>
+      </div>
+    </section>
+    <NewPortfolio />
+  </main>
 </template>
