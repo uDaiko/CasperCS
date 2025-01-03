3ae9639850f2468eece998b75212cf10388f1bf1
uDaiko <uDegr@proton.me>
2025-01-04 00:21:17 +0900

    config changes for devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   modules: ["@nuxt/ui", "@nuxtjs/supabase", "@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
@@ -13,5 +19,9 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     STOCKAPI_KEY: process.env.STOCKAPI_KEY,
+
+    public: {
+      baseUrl: process.env.BASE_URL ?? "http://localhost:3000",
+    },
   },
 });
