71cec1fa7c50c1247315b70f43b8316d34b0fcdf
uDaiko <uDegr@proton.me>
2025-01-06 13:58:23 +0900

    added props to pass and changed to NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -16,9 +16,13 @@ onBeforeMount(async () => {
   <main class="container mx-auto px-4 py-8">
     <section class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
       <div v-for="portfolio in portfolios" :key="portfolio.id">
-        <router-link :to="`/portfolio/${portfolio.name}`">
-          <InvestmentSummary :type="portfolio.type" :name="portfolio.name" />
-        </router-link>
+        <NuxtLink :to="`/portfolio/${portfolio.name}`">
+          <InvestmentSummary
+            :type="portfolio.type"
+            :name="portfolio.name"
+            :portfolio-id="portfolio.id"
+          />
+        </NuxtLink>
       </div>
     </section>
     <NewPortfolio />
