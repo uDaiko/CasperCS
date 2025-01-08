c4975ba8ea1b90f845996e4c9fdb43ed382fe941
uDaiko <uDegr@proton.me>
2025-01-09 03:56:38 +0900

    modal form for portfolio creation

---
+++ components/portfolio-modal.vue
@@ -0,0 +1,28 @@
+<script setup>
+const portfolioCategories = ["Stocks", "Crypto"];
+</script>
+
+<template>
+  <UModal v-model="isOpen">
+    <UCard>
+      <h2>Add a new portfolio</h2>
+
+      <UForm>
+        <UFormGroup
+          label="Portfolio name"
+          name="name"
+          class="mb-4"
+          :required="true"
+        >
+          <UInput placeholder="What should the portfolio be called" />
+        </UFormGroup>
+
+        <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+          <USelect placeholder="For what asset class is this portfolio?" />
+        </UFormGroup>
+
+        <UButton variant="solid" label="Save" type="submit" />
+      </UForm>
+    </UCard>
+  </UModal>
+</template>
