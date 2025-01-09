7666b69e4377604a12420a4324c41d03fe8d088a
uDaiko <uDegr@proton.me>
2025-01-10 04:12:06 +0900

    added v model functionality

--- components/asset-modal.vue
+++ components/asset-modal.vue
@@ -8,3 +8,11 @@ const isModalOpen = computed({
   set: (value) => emit("update:modelValue", value),
 });
 </script>
+
+<template>
+  <UModal v-model="isModalOpen">
+    <UCard>
+      <h2 class="mb-3">Add a new asset</h2>
+    </UCard>
+  </UModal>
+</template>
