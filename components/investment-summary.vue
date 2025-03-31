d862306172a56b1f6751b66d1205cf65fbbe417e
uDaiko <uDegr@proton.me>
2025-03-31 23:43:35 +0200

    added style choice when adding a new portfolio

--- components/investment-summary.vue
+++ components/investment-summary.vue
@@ -4,19 +4,38 @@ const props = defineProps<{
   type: InvestmentType;
   name: string;
   portfolioId: string;
+  style: string;
 }>();
+
 const rawassetList = ref([]);
 const isLoading = ref(true)
 const totalResults = ref([])
 const supabase = useSupabaseClient();
 
-const fetchAssetData = async() => {
-  const { data } = await supabase
-  .from("stocks")
-  .select()
-  .eq("portfolio_id", props.portfolioId);
+const getGradientClass = computed(() => {
+  const styleValue = props.style;
 
-rawassetList.value = data;
+  switch (styleValue) {
+    case '0':
+      return 'bg-gradient-to-br from-purple-600 to-blue-400';
+    case '1':
+      return 'bg-gradient-to-br from-red-600 to-pink-500';
+    case '2':
+      return 'bg-gradient-to-br from-blue-600 to-green-400';
+    case '3':
+      return 'bg-gradient-to-br from-orange-600 to-yellow-500';
+    default:
+      return 'bg-gradient-to-br from-red-600 to-orange-600';
+  }
+});
+
+const fetchAssetData = async () => {
+  const { data } = await supabase
+    .from("stocks")
+    .select()
+    .eq("portfolio_id", props.portfolioId);
+
+  rawassetList.value = data;
 
 
   const assetPromises = rawassetList.value.map(async (asset) => {
@@ -44,23 +63,17 @@ const portfolioValue = computed(() => {
 </script>
 
 <template>
-  <div
-    class="bg-gradient-to-br from-purple-600 to-indigo-800 rounded-xl p-4 shadow-lg"
-    v-if="type === 'Crypto'"
-  >
+  <div :class="[getGradientClass, 'rounded-xl p-4 shadow-lg']" v-if="type === 'Crypto'">
     <div class="flex justify-between items-center mb-4">
-      <span class="text-lg font-semibold">{{ name }} Portfolio</span>
+      <span class="text-lg font-semibold">{{ name }} Crypto Portfolio</span>
       <span class="text-green-300"></span>
     </div>
     <div class="text-4xl font-bold mb-4">$24,369.41</div>
   </div>
 
-  <div
-    class="bg-gradient-to-br from-red-600 to-yellow-800 rounded-xl p-4 shadow-lg"
-    v-if="type === 'Stocks'"
-  >
+  <div :class="[getGradientClass, 'rounded-xl p-4 shadow-lg']" v-if="type === 'Stocks'">
     <div class="flex justify-between items-center mb-4">
-      <span class="text-lg font-semibold">{{ name }} portfolio</span>
+      <span class="text-lg font-semibold">{{ name }} Stock Portfolio</span>
       <span class="text-green-300"></span>
     </div>
     <div class="text-4xl font-bold mb-4">
--- components/portfolio-modal.vue
+++ components/portfolio-modal.vue
@@ -1,6 +1,12 @@
 <script setup>
 const supabase = useSupabaseClient();
 const portfolioCategories = ["Stocks", "Crypto"];
+const portfolioStyles = [
+  { name: "Purple to Indigo", value: 0, gradient: "from-purple-600 to-blue-400" },
+  { name: "Red to Yellow", value: 1, gradient: "from-red-600 to-pink-500" },
+  { name: "Blue to Green", value: 2, gradient: "from-blue-600 to-green-400" },
+  { name: "Orange to Yellow", value: 3, gradient: "from-orange-600 to-yellow-500" }
+];
 const props = defineProps({
   modelValue: Boolean,
 });
@@ -9,6 +15,7 @@ const toast = useToast();
 
 const name = ref();
 const category = ref();
+const style = ref();
 let isDisabled = ref(false);
 
 const isModalOpen = computed({
@@ -24,6 +31,7 @@ const save = async () => {
       id: undefined,
       name: name.value,
       type: category.value,
+      style: style.value.toString(),
       user_id: undefined,
     });
     if (error) throw error;
@@ -47,38 +55,27 @@ const save = async () => {
       <h2 class="mb-3">Add a new portfolio</h2>
 
       <UForm>
-        <UFormGroup
-          label="Portfolio Name"
-          name="name"
-          class="mb-4"
-          :required="true"
-        >
-          <UInput
-            placeholder="What should the portfolio be called"
-            v-model="name"
-          />
+        <UFormGroup label="Portfolio Name" name="name" class="mb-4" :required="true">
+          <UInput placeholder="What should the portfolio be called" v-model="name" />
         </UFormGroup>
 
-        <UFormGroup
-          label="Portfolio Category"
-          :required="true"
-          name="portfolioCategory"
-          class="mb-4"
-        >
-          <USelect
-            placeholder="For what asset class is this portfolio?"
-            :options="portfolioCategories"
-            v-model="category"
-          />
+        <UFormGroup label="Portfolio Category" :required="true" name="portfolioCategory" class="mb-4">
+          <USelect placeholder="For what asset class is this portfolio?" :options="portfolioCategories"
+            v-model="category" />
         </UFormGroup>
 
-        <UButton
-          color="black"
-          label="Save"
-          type="submit"
-          @click="save"
-          :disabled="isLoading"
-        />
+        <UFormGroup label="Portfolio Style" :required="true" name="portfolioStyle" class="mb-4">
+          <div class="flex gap-4">
+            <div v-for="styleOption in portfolioStyles" :key="styleOption.value" class="relative cursor-pointer"
+              @click="style = styleOption.value">
+              <div class="w-12 h-12 rounded-full bg-gradient-to-br"
+                :class="[styleOption.gradient, { 'border-2 border-green-500': style === styleOption.value }]">
+              </div>
+            </div>
+          </div>
+        </UFormGroup>
+
+        <UButton color="black" label="Save" type="submit" @click="save" :disabled="isLoading" />
       </UForm>
     </UCard>
   </UModal>
