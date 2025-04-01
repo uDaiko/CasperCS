8f651bb342ca41f24fd5ccd5f7bbb97d77ac3c18
uDaiko <uDegr@proton.me>
2025-04-01 16:08:11 +0200

    added a quick login button for the test user

--- pages/login.vue
+++ pages/login.vue
@@ -6,6 +6,7 @@ const password = ref("");
 const supabaseClient = useSupabaseClient();
 const toast = useToast();
 const user = useSupabaseUser();
+const config = useRuntimeConfig();
 
 const handleLogin = async () => {
   if (!email.value || !password.value) {
@@ -19,7 +20,7 @@ const handleLogin = async () => {
   }
   isLoading.value = true;
   try {
-    const { data, error } = await supabaseClient.auth.signInWithPassword({
+    const { error } = await supabaseClient.auth.signInWithPassword({
       email: email.value,
       password: password.value,
     });
@@ -45,6 +46,34 @@ const handleLogin = async () => {
   }
 };
 
+const handleTestLogin = async () => {
+  isLoading.value = true;
+  try {
+    const { error } = await supabaseClient.auth.signInWithPassword({
+      email: config.public.TEST_USER_EMAIL,
+      password: config.public.TEST_USER_PASSWORD,
+    });
+
+    if (error) throw error;
+
+    toast.add({
+      title: "Guest Login Successful",
+      icon: "i-heroicons-check-circle",
+      color: "green",
+    });
+    navigateTo('/');
+  } catch (error) {
+    console.error(error);
+    toast.add({
+      title: "guest login Unsuccessful",
+      icon: "i-heroicons-exclamation-circle",
+      color: "red",
+    });
+  } finally {
+    isLoading.value = false;
+  }
+};
+
 watch(
   user,
   (user) => {
@@ -60,30 +89,20 @@ watch(
   <UCard v-if="!user" class="bg-slate-700 mt-6">
     <template #header>Sign-in</template>
     <form @submit.prevent="handleLogin">
-      <UFormGroup
-        class="mb-4"
-        label="Email"
-        name="email"
-        :required="true"
-        help="After signing in you will be redirected to the main page"
-      >
-        <UInput
-          type="email"
-          placeholder="Type in your email"
-          class="bg-black mb-2"
-          required="true"
-          v-model="email"
-        />
-        <UInput
-          type="password"
-          placeholder="Type in your password"
-          class="bg-black"
-          required="true"
-          v-model="password"
-        />
+      <UFormGroup class="mb-4" label="Email" name="email" :required="true"
+        help="After signing in you will be redirected to the main page">
+        <UInput type="email" placeholder="Type in your email" class="bg-black mb-2" required="true" v-model="email" />
+        <UInput type="password" placeholder="Type in your password" class="bg-black" required="true"
+          v-model="password" />
       </UFormGroup>
       <UButton type="submit" variant="solid">Login</UButton>
     </form>
+    <div class="mt-4 pt-4 border-t border-gray-600">
+      <p class="text-sm text-gray-400 mb-2">If you want to try out the application click here!</p>
+      <UButton @click="handleTestLogin" variant="solid" color="gray">
+        Login as Test User
+      </UButton>
+    </div>
   </UCard>
   <UCard v-else>
     <template #header>
