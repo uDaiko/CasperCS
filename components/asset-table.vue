4dea92da587785d3cd52b206cac002204768b966
uDaiko <uDegr@proton.me>
2025-04-15 23:41:07 +0200

    design rework for table

--- components/asset-table.vue
+++ components/asset-table.vue
@@ -60,14 +60,32 @@ const handleDelete = (id: number) => {
 </script>
 
 <template>
-  <UTable :columns="columns" :rows="stockDataRows">
+  <UTable :columns="columns" :rows="stockDataRows" class="mt-4" :ui="{
+    th: {
+      base: ' bg-slate-900',
+    },
+    td: {
+      base: 'text-slate-300 bg-slate-950',
+    },
+    wrapper: 'rounded-lg border border-slate-800',
+  }">
+
+    <template #total-data="{ row }">
+      <span class="text-green-500">{{ row.total }}</span>
+    </template>
     <template #actions-data="{ row }">
       <UDropdown :items="[[{
         label: 'Delete',
         icon: 'i-heroicons-trash',
-        click: () => handleDelete(row.id)
-      }]]">
-        <UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal" />
+        click: () => handleDelete(row.id),
+        class: 'text-red-500'
+      }]]" :ui="{
+        background: 'bg-slate-900',
+        item: {
+          base: 'text-slate-300 hover:bg-slate-800/50'
+        }
+      }">
+        <UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal" class="text-slate-400" />
       </UDropdown>
     </template>
   </UTable>
